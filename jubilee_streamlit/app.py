78a0ee58a89b66a920f518ad19bbb82c66145866
UltraGeek0102 <Dhruv.bosamia@gmail.com>
2025-07-05 17:19:01 +0530

    Update app.py

--- jubilee_streamlit/app.py
+++ jubilee_streamlit/app.py
@@ -15,7 +15,7 @@ CREDENTIALS_FILE = "creds.json"  # Replace with your credentials
 
 # Setup Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
 client = gspread.authorize(creds)
 sheet = client.open(SHEET_NAME).sheet1
 
